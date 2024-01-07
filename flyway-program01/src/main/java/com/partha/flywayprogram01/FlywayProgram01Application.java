0538fd30bcf686b64756baff34f8c1e89b09031d
partha biswas <biswas.partha1@gmail.com>
2024-01-08 02:28:19 +0530

    initial commit

---
+++ flyway-program01/src/main/java/com/partha/flywayprogram01/FlywayProgram01Application.java
@@ -0,0 +1,13 @@
+package com.partha.flywayprogram01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayProgram01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayProgram01Application.class, args);
+	}
+
+}
