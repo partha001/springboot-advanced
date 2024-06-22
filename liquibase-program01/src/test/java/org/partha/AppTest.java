26b50294df9cf4136d0b5bf9b164fbb52b3d17c6
Partha Biswas <biswas.partha1@gmail.com>
2024-06-22 14:25:08 +0530

    liquibase : database migration with liquibase

---
+++ liquibase-program01/src/test/java/org/partha/AppTest.java
@@ -0,0 +1,14 @@
+package org.partha;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class AppTest {
+
+    @Test
+    void test(){
+        Assertions.assertTrue(true);
+    }
+
+}
